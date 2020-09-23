1d2fff38bc418072ee5fecf727b69e807309755d
Thomas Helmke <thomas.helmke@gmx.de>
2020-09-23 21:05:10 +0200

    Version mit Interrupt auf ESP8266 und MQTT

---
+++ version3/tcs-interrupt/tcs-interrupt.ino
@@ -0,0 +1,380 @@
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino
+
+//needed for library
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+
+#include <PubSubClient.h>
+
+WiFiClient net;
+PubSubClient client(net);
+
+//define your default values here, if there are different values in config.json, they are overwritten.
+char mqtt_server[40];
+char mqtt_port[6] = "8080";
+char mqtt_topic[34] = "home/flur/klingel";
+char mqtt_client[16] = "klingel";
+char mqtt_user[16] = "user";
+char mqtt_pass[16] = "pass";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+#define inputPin 14 // D5
+#define startBit 6
+#define oneBit 4
+#define zeroBit 2
+
+volatile uint32_t CMD = 0;
+volatile uint8_t lengthCMD = 0;
+volatile bool cmdReady;
+
+void connectToWifi()
+{
+    Serial.print("checking wifi...");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(1000);
+    }
+
+    connectToMqtt();
+
+    Serial.println("\nconnected!");
+}
+
+void connectToMqtt()
+{
+    Serial.print("\nconnecting to MQTT...");
+    while (!client.connect(mqtt_client, mqtt_user, mqtt_pass))
+    {
+        Serial.print(".");
+        delay(5000);
+    }
+}
+
+void sendMessage()
+{
+    // connectToMqtt();
+    client.publish(mqtt_topic, "triggered");
+    // delay(100);
+    // client.disconnect();
+}
+
+//callback notifying us of the need to save config
+void saveConfigCallback()
+{
+    Serial.println("Should save config");
+    shouldSaveConfig = true;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+}
+
+void ICACHE_RAM_ATTR analyzeCMD()
+{
+    static uint32_t curCMD;
+    static uint32_t usLast;
+    static byte curCRC;
+    static byte calCRC;
+    static byte curLength;
+    static byte cmdIntReady;
+    static byte curPos;
+    uint32_t usNow = micros();
+    uint32_t timeInUS = usNow - usLast;
+    usLast = usNow;
+    byte curBit = 4;
+    if (timeInUS >= 1000 && timeInUS <= 2999)
+    {
+        curBit = 0;
+    }
+    else if (timeInUS >= 3000 && timeInUS <= 4999)
+    {
+        curBit = 1;
+    }
+    else if (timeInUS >= 5000 && timeInUS <= 6999)
+    {
+        curBit = 2;
+    }
+    else if (timeInUS >= 7000 && timeInUS <= 24000)
+    {
+        curBit = 3;
+        curPos = 0;
+    }
+
+    if (curPos == 0)
+    {
+        if (curBit == 2)
+        {
+            curPos++;
+        }
+        curCMD = 0;
+        curCRC = 0;
+        calCRC = 1;
+        curLength = 0;
+    }
+    else if (curBit == 0 || curBit == 1)
+    {
+        if (curPos == 1)
+        {
+            curLength = curBit;
+            curPos++;
+        }
+        else if (curPos >= 2 && curPos <= 17)
+        {
+            if (curBit)
+                bitSet(curCMD, (curLength ? 33 : 17) - curPos);
+            calCRC ^= curBit;
+            curPos++;
+        }
+        else if (curPos == 18)
+        {
+            if (curLength)
+            {
+                if (curBit)
+                    bitSet(curCMD, 33 - curPos);
+                calCRC ^= curBit;
+                curPos++;
+            }
+            else
+            {
+                curCRC = curBit;
+                cmdIntReady = 1;
+            }
+        }
+        else if (curPos >= 19 && curPos <= 33)
+        {
+            if (curBit)
+                bitSet(curCMD, 33 - curPos);
+            calCRC ^= curBit;
+            curPos++;
+        }
+        else if (curPos == 34)
+        {
+            curCRC = curBit;
+            cmdIntReady = 1;
+        }
+    }
+    else
+    {
+        curPos = 0;
+    }
+    if (cmdIntReady)
+    {
+        cmdIntReady = 0;
+        if (curCRC == calCRC)
+        {
+            cmdReady = 1;
+            lengthCMD = curLength;
+            CMD = curCMD;
+        }
+        curCMD = 0;
+        curPos = 0;
+    }
+}
+
+void printHEX(uint32_t DATA)
+{
+    uint8_t numChars = lengthCMD ? 8 : 4;
+    uint32_t mask = 0x0000000F;
+    mask = mask << 4 * (numChars - 1);
+    for (uint32_t i = numChars; i > 0; --i)
+    {
+        Serial.print(((DATA & mask) >> (i - 1) * 4), HEX);
+        mask = mask >> 4;
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    // SPIFFS.format();
+    pinMode(inputPin, INPUT);
+    attachInterrupt(digitalPinToInterrupt(inputPin), analyzeCMD, CHANGE);
+    //read configuration from FS json
+    Serial.println("mounting FS...");
+
+    if (SPIFFS.begin())
+    {
+        Serial.println("mounted file system");
+        if (SPIFFS.exists("/config.json"))
+        {
+            //file exists, reading and loading
+            Serial.println("reading config file");
+            File configFile = SPIFFS.open("/config.json", "r");
+            if (configFile)
+            {
+                Serial.println("opened config file");
+                size_t size = configFile.size();
+                // Allocate a buffer to store contents of the file.
+                std::unique_ptr<char[]> buf(new char[size]);
+
+                configFile.readBytes(buf.get(), size);
+                DynamicJsonDocument json(1024);
+                DeserializationError error = deserializeJson(json, buf.get());
+                serializeJson(json, Serial);
+                if (!error)
+                {
+                    Serial.println("\nparsed json");
+
+                    strcpy(mqtt_server, json["mqtt_server"]);
+                    strcpy(mqtt_port, json["mqtt_port"]);
+                    strcpy(mqtt_topic, json["mqtt_topic"]);
+                    strcpy(mqtt_client, json["mqtt_client"]);
+                    strcpy(mqtt_user, json["mqtt_user"]);
+                    strcpy(mqtt_pass, json["mqtt_pass"]);
+                }
+                else
+                {
+                    Serial.println("failed to load json config");
+                }
+            }
+        }
+    }
+    else
+    {
+        Serial.println("failed to mount FS");
+    }
+    //end read
+
+    // The extra parameters to be configured (can be either global or just in the setup)
+    // After connecting, parameter.getValue() will get you the configured value
+    // id/name placeholder/prompt default length
+    WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+    WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
+    WiFiManagerParameter custom_mqtt_topic("topic", "mqtt topic", mqtt_topic, 32);
+    WiFiManagerParameter custom_mqtt_client("client", "mqtt client", mqtt_client, 32);
+    WiFiManagerParameter custom_mqtt_user("user", "mqtt user", mqtt_user, 32);
+    WiFiManagerParameter custom_mqtt_pass("pass", "mqtt pass", mqtt_pass, 32);
+
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+
+    //set config save notify callback
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+    //set static ip
+    // wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+    //add all your parameters here
+    wifiManager.addParameter(&custom_mqtt_server);
+    wifiManager.addParameter(&custom_mqtt_port);
+    wifiManager.addParameter(&custom_mqtt_topic);
+    wifiManager.addParameter(&custom_mqtt_client);
+    wifiManager.addParameter(&custom_mqtt_user);
+    wifiManager.addParameter(&custom_mqtt_pass);
+
+    //reset settings - for testing
+    //wifiManager.resetSettings();
+
+    //set minimu quality of signal so it ignores AP's under that quality
+    //defaults to 8%
+    //wifiManager.setMinimumSignalQuality();
+
+    //sets timeout until configuration portal gets turned off
+    //useful to make it all retry or go to sleep
+    //in seconds
+    //wifiManager.setTimeout(120);
+
+    //fetches ssid and pass and tries to connect
+    //if it does not connect it starts an access point with the specified name
+    //here  "AutoConnectAP"
+    //and goes into a blocking loop awaiting configuration
+    if (!wifiManager.autoConnect("KlingelAP", "password"))
+    {
+        Serial.println("failed to connect and hit timeout");
+        delay(3000);
+        //reset and try again, or maybe put it to deep sleep
+        ESP.reset();
+        delay(5000);
+    }
+
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+    //read updated parameters
+    strcpy(mqtt_server, custom_mqtt_server.getValue());
+    strcpy(mqtt_port, custom_mqtt_port.getValue());
+    strcpy(mqtt_topic, custom_mqtt_topic.getValue());
+    strcpy(mqtt_client, custom_mqtt_client.getValue());
+    strcpy(mqtt_user, custom_mqtt_user.getValue());
+    strcpy(mqtt_pass, custom_mqtt_pass.getValue());
+
+    //save the custom parameters to FS
+    if (shouldSaveConfig)
+    {
+        Serial.println("saving config");
+        DynamicJsonDocument json(1024);
+        json["mqtt_server"] = mqtt_server;
+        json["mqtt_port"] = mqtt_port;
+        json["mqtt_topic"] = mqtt_topic;
+        json["mqtt_client"] = mqtt_client;
+        json["mqtt_user"] = mqtt_user;
+        json["mqtt_pass"] = mqtt_pass;
+
+        File configFile = SPIFFS.open("/config.json", "w");
+        if (!configFile)
+        {
+            Serial.println("failed to open config file for writing");
+        }
+
+        serializeJson(json, Serial);
+        serializeJson(json, configFile);
+        configFile.close();
+        //end save
+    }
+
+    Serial.println("local ip");
+    Serial.println(WiFi.localIP());
+
+    Serial.print("MQTT Server ");
+    Serial.println(mqtt_server);
+    Serial.print("MQTT port ");
+    Serial.println(atoi(mqtt_port));
+
+    client.setServer(mqtt_server, atoi(mqtt_port));
+    client.setCallback(callback);
+
+    connectToWifi();
+}
+
+void loop()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        connectToWifi();
+    }
+    if (!client.connected())
+    {
+        connectToMqtt();
+    }
+    client.loop();
+    if (cmdReady)
+    {
+        cmdReady = 0;
+        Serial.write(0x01);
+        Serial.print("$");
+        printHEX(CMD);
+        Serial.write(0x04);
+        Serial.println();
+
+        char byte_cmd[9];
+        sprintf(byte_cmd, "%04x", CMD);
+        client.publish(mqtt_topic, byte_cmd);
+    }
+}
