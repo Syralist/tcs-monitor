710ec064801e562a41fa2dd4dab670743d4129ee
Thomas Helmke <thomas.helmke@gmx.de>
2020-09-24 17:49:22 +0200

    OTA hinzugefügt

--- version3/tcs-interrupt/tcs-interrupt.ino
+++ version3/tcs-interrupt/tcs-interrupt.ino
@@ -7,6 +7,10 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
 
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 #include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
 
 #include <PubSubClient.h>
@@ -350,6 +354,51 @@ void setup()
     client.setServer(mqtt_server, atoi(mqtt_port));
     client.setCallback(callback);
 
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+        {
+            type = "sketch";
+        }
+        else
+        { // U_FS
+            type = "filesystem";
+        }
+
+        // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+        {
+            Serial.println("Auth Failed");
+        }
+        else if (error == OTA_BEGIN_ERROR)
+        {
+            Serial.println("Begin Failed");
+        }
+        else if (error == OTA_CONNECT_ERROR)
+        {
+            Serial.println("Connect Failed");
+        }
+        else if (error == OTA_RECEIVE_ERROR)
+        {
+            Serial.println("Receive Failed");
+        }
+        else if (error == OTA_END_ERROR)
+        {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
+
     connectToWifi();
 }
 
@@ -364,6 +413,7 @@ void loop()
         connectToMqtt();
     }
     client.loop();
+    ArduinoOTA.handle();
     if (cmdReady)
     {
         cmdReady = 0;
